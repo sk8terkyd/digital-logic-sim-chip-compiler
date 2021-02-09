28deac4a02e833479c3b6c5442b49222ccee5dd7
sk8terkyd <73844697+sk8terkyd@users.noreply.github.com>
2021-02-09 18:23:39 -0500

    This is the first version
    "NAND.txt" and "OR.txt" are for testing

---
+++ Chip.py
@@ -0,0 +1,66 @@
+################################################################################
+############################ Chip Class ########################################
+################################################################################
+
+
+import json
+
+# Definition:   function read_file(string)
+# Arguments:    string file_path
+# Purpose:      Reads file specified by path given, returns
+#               string with contents
+# Notes:        Couldn't possibly be safer ;)
+def read_file(file_path):
+    try:
+        file = open(file_path, 'r')
+        try:
+            return file.read()
+        except:
+            print(f"Could not read file! Path to requested file: {file_path}")
+            quit(1)
+        finally:
+            file.close()
+    except:
+        print(f"Requested file not found! Path to requested file: {file_path}")
+        quit(1)
+
+
+# Definition:   function read_json(string)
+# Arguments:    string json_string
+# Purpose:      Takes in contents of file written in JSON,
+#               turns it into Python's dictionary type
+# Notes:        Error handling added
+def read_json(json_string):
+    try:
+        return json.loads(json_string)
+    except:
+        print(f"Could not parse contents of file! Contents: {json_string}")
+        quit(1)
+
+
+# Definition:   class Chip(string)
+# Arguments:    string file_path
+# Purpose:      Packs all internals of chip into one neat object
+# Notes:        It seems to be safe (catches all exceptions)
+class Chip:
+    def __init__(self, file_path):
+        # initializing the values that matter to us
+        self.path = file_path
+        try:
+            self.json_text = read_file(self.path)
+        except:
+            print(f"Needed chip file not found! Path to chip file: {file_path}")
+            quit(1)
+        self.dict_with_parts = read_json(self.json_text)
+        # name of chip
+        self.name = self.dict_with_parts["name"]
+        # components that the chip is made of
+        self.component_name_list = self.dict_with_parts["componentNameList"]
+        # actual components section of chip
+        self.components = self.dict_with_parts["savedComponentChips"]
+
+    # returning the important values
+    def __repr__(self):
+        return repr(f"""name: {self.name},""" +
+                    f"""component names: {self.component_name_list},""" +
+                    f"""components: {self.components}""")
---
+++ main.py
@@ -0,0 +1,90 @@
+################################################################################
+################################## READ ME #####################################
+################################################################################
+"""
+If at any point you are looking at this code and you DONT know what it does OR
+you have a question about it, Tell Me so i can explain better what it does. also
+if you change anything put a comment that you changed it please
+"""
+
+# import json module, for reading save files
+import json
+# import sys module, for command line
+import sys
+# import Chip class
+from Chip import Chip
+
+
+# Definition:   function create_chip_file(Chip, string)
+# Arguments:    Chip chip_obj, string file_name
+# Purpose:      Saves Chip object into file with path
+#               equal to file_name.
+# Notes:        Find out how to set "creationIndex" value!!!
+#               Also, currently no way to set colour - default is blue.
+def create_chip_file(chip_obj, file_name):
+    try:
+        create_chip = open(file_name, "w")
+        try:
+            json.dump({
+                "name": (chip_obj.name),
+                "creationIndex": 0,
+                "colour": {
+                    "r": 0.0,
+                    "g": 0.0,
+                    "b": 1.0,
+                    "a": 1.0
+                },
+                "nameColour": {
+                    "r": 1.0,
+                    "g": 1.0,
+                    "b": 1.0,
+                    "a": 1.0
+                },
+                "componentNameList": (chip_obj.component_name_list),
+                "savedComponentChips": (chip_obj.components)
+            }, create_chip, indent = 4)
+        except:
+            print(f"Could not dump chip! Printable version: {repr(chip_obj)}")
+            quit(1)
+    except:
+        print(f"Could not open output file! Requested file: {file_name}")
+        quit(1)
+
+
+# Definition:   function create_new_chip(Chip)
+# Arguments:    Chip chip
+# Purpose:      Recursive function, which resolves any
+#               possible dependencies on chip
+# Notes:        Second argument MUST BE LEFT TO DEFAULT!!!
+#               It is used internally, as a saved version of
+#               original chip argument
+def create_new_chip(chip, return_chip = None):
+    builtin_components = ["AND", "NOT", "SIGNAL IN", "SIGNAL OUT"]
+    other_components = []
+
+    # if its the first iteration of recursion then return chip = chip
+    if return_chip == None:
+        return_chip = chip
+
+    # Check if the chip has other chips inside it
+    for component in chip.component_name_list:
+        if component not in builtin_components:
+            # move component from component_name_list to other_components
+            other_components.append(component)
+            return_chip.component_name_list.remove(component)
+
+    # possibly unnecessary?
+    return_chip.component_name_list = builtin_components
+
+    # if our chip is made of only and/not gates, return "return_chip" untouched
+    if other_components == []:
+        return return_chip
+
+    # otherwise, recursively try to insert new chips
+    else:
+        # TODO: write code that appends new chip to old one.
+
+# TESTING
+chip_test = Chip("OR.txt")
+new_chip = create_new_chip(chip_test)
+print(repr(new_chip))
