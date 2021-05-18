150d41927f62f637b778c05b9afd9e3a4b1bd6fe
William LIAO <william.bw.liao@gmail.com>
2021-05-18 10:32:08 -0400

    Make error messages for main,py

--- main.py
+++ main.py
@@ -9,8 +9,15 @@ from get_others import get_others
 # import create_chip
 from create_chip import create_new_chip
 
+if len(sys.argv) < 3:
+    print("Please provide two arguments.")
 
-originalChip = Chip(sys.argv[1])
-baseChip = create_new_chip(originalChip)
-create_chip_file(baseChip, sys.argv[2])
+try:
+    originalChip = Chip(sys.argv[1])
+    baseChip = create_new_chip(originalChip)
+    create_chip_file(baseChip, sys.argv[2])
+except:
+    print("Please provide valid paths for both arguments.")
+    quit()
+
 print("Your simplified chip save has been made.")
